dd00e8534ce08d330f406672c834405249369c8a
jakariamasum <jmphy698@gmail.com>
2023-06-12 21:47:21 +0600

    manage user and manage class added

--- src/Pages/Admin/ManageClass/ManageClass.jsx
+++ src/Pages/Admin/ManageClass/ManageClass.jsx
@@ -1,8 +1,62 @@
+import { useContext, useEffect, useState } from "react";
+import { IoMdRefresh } from 'react-icons/io'
+import { RxCross2 } from 'react-icons/rx'
+import { BsCheckCircle } from 'react-icons/bs'
+import { FaCommentAlt } from 'react-icons/fa'
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const ManageClass = () => {
+    const { user } = useContext(AuthContext);
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/add-class`)
+            .then((res) => res.json())
+            .then((data) => setClasses(data));
+    }, [user?.email]);
+
+
     return (
         <div>
-            <h1>Manage Classes</h1>
+            <h1 className="text-2xl font-bold mb-4">
+                Total Selected: {classes.length}
+            </h1>
+            <table className="table text-center">
+                <thead>
+                    <tr>
+                        <th>Serial Number</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Instructor Name</th>
+                        <th>Instructor Email</th>
+                        <th>Price</th>
+                        <th>Seats</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {classes.map((classItem, index) => (
+                        <tr key={classItem.id}>
+                            <td>{index + 1}</td>
+                            <td>
+                                <img src={classItem.image} alt={classItem.name} className="mask mask-squircle w-12 h-12" />
+                            </td>
+                            <td>{classItem.name}</td>
+                            <td>{classItem.instructor}</td>
+                            <td>{classItem.email}</td>
+                            <td>${classItem.price}</td>
+                            <td>{classItem.availableSeats}</td>
+                            <td className="text-yellow-400">{classItem.status}</td>
+                            <td className="flex items-center gap-1">
+                                <button className="btn btn-warning mr-2 font-semibold border-none text-white bg-green-400 hover:bg-purple-500"><BsCheckCircle size={20} /> Approve</button>
+                                <button className="btn btn-warning mr-2 font-semibold border-none text-white bg-red-400 hover:bg-purple-500"><RxCross2 size={20} /> Deny</button>
+                                <button className="btn btn-warning mr-2 font-semibold border-none text-white bg-orange-400 hover:bg-purple-500"><FaCommentAlt size={20} /> FeedBack</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/Admin/ManageUsers/ManageUsers.jsx
+++ src/Pages/Admin/ManageUsers/ManageUsers.jsx
@@ -1,10 +1,56 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import {TiUserAdd} from 'react-icons/ti'
+import {FaUserCheck} from 'react-icons/fa'
+import { Link } from "react-router-dom";
 
 const ManageUsers = () => {
-    return (
-        <div>
-            <h1>all user</h1>
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/students`)
+      .then((res) => res.json())
+      .then((data) => setClasses(data));
+  }, [user?.email]);
+  
+
+  return (
+    <div>
+        <h1 className="text-2xl font-bold mb-4">
+            Total Selected: {classes.length}
+        </h1>
+      <table className="table text-center">
+        <thead>
+          <tr>
+            <th>Serial Number</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Instructor Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {classes.map((classItem, index) => (
+            <tr key={classItem.id}>
+              <td>{index + 1}</td>
+              <td>
+              <img src={classItem.image} alt={classItem.name} className="mask mask-squircle w-12 h-12" />
+              </td>
+              <td>{classItem.name}</td>
+              <td>{classItem.instructor}</td>
+              <td className="flex items-center">
+                <button className="btn btn-warning mr-2 border-none text-white bg-green-400 hover:bg-purple-500"><TiUserAdd size={25}/> Make Admin</button>
+                <Link to={`/student-dashboard/${classItem._id}`}>
+                    <button className="btn  mr-2 border-none text-white bg-orange-400 hover:bg-gray-600"><FaUserCheck size={20}/> Make Instructor</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ManageUsers;
